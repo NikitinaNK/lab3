ea85dd7f451bcc6b11320e8c89a77c6444b0734a
Natasha <iiam_nikitina@icloud.com>
2021-05-23 00:33:05 +0300

    code: color

--- laba3.cpp
+++ laba3.cpp
@@ -2,7 +2,9 @@
 //
 
 #include <iostream>
+#include <string>
 #include <vector>
+
 
 using namespace std;
 
@@ -48,8 +50,52 @@ vector<size_t>make_histogram(const vector<double>& numbers, size_t count, double
     return bins;
 }
 
+void
+svg_text(double left, double baseline, string text) {
+    cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>";
+}
+void
+svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void
+svg_end() {
+    cout << "</svg>\n";
+}
+void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black")
+{
+    cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill= '" << fill <<"' />";
+}
+void
+show_histogram_svg(const vector<size_t>& bins) {
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    svg_begin(400, 300);
+    svg_text(20, 20, to_string(bins[0]));
+    svg_rect(50, 0, bins[0] * 10, 30, "red" );
+    double top = 0;
+    for (size_t bin : bins) {
+        const double bin_width = BLOCK_WIDTH * bin;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "red");
+        top += BIN_HEIGHT;
+    }
+    svg_end();
+}
 void show_histogram_text(vector<size_t> bins)
 {
+
     // Вывод данных
     const size_t SCREEN_WIDTH = 80;
     const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
@@ -90,10 +136,11 @@ void show_histogram_text(vector<size_t> bins)
         cout << '\n';
     }
 }
+
 int main()
 {
     size_t number_count;
-    cout << "Enter number count: ";
+    cerr << "Enter number count: ";
     cin >> number_count;
 
     // Ввод чисел заменен вызовом функции:
@@ -108,7 +155,7 @@ int main()
     find_minmax(numbers, min, max);
 
     const auto bins = make_histogram(numbers, bin_count, min, max);
-    show_histogram_text(bins);
+    show_histogram_svg(bins);
 
 
     return 0;
